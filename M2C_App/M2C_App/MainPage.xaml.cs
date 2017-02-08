80123cffae9933933e97b4bb1be0f18cbc5b660c
guzquiano <guzquiano@EXM2C008>
2017-02-08 19:42:59 +0100

    Agregar archivos de proyecto.

---
+++ M2C_App/M2C_App/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace M2C_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //InitializeComponent();
+
+            Label header = new Label
+            {
+                Text = "Intranet M2C",
+                FontSize = 20,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.Blue,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            WebView webView = new WebView
+            {
+                Source = new UrlWebViewSource
+                {
+                    Url = "http://www.m2c.es/es/SitePages/home.aspx",
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    webView
+                }
+            };
+        }
+    }
+}
